f82688eb23e5bbd65076c8d000327f1d4d90ae0d
Tommaso Amorosi <tomy.bawse@gmail.com>
2023-05-27 13:36:49 +0200

    First commit

--- main.go
+++ main.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-type Simple struct {
-	Name        string
-	Description string
-	Url         string
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	simple := Simple{"Hello", "World", r.Host}
-
-	jsonOutput, _ := json.Marshal(simple)
-
-	fmt.Fprintln(w, string(jsonOutput))
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
 func main() {
-	fmt.Println("Server started on port 4444")
-	http.HandleFunc("/api", handler)
-	log.Fatal(http.ListenAndServe(":4444", nil))
+    handleRequests()
 }
