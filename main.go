bc5edde03c795f6161218004fa4167fa35d532c5
abinashp-github <95472074+abinashp-github@users.noreply.github.com>
2023-06-02 22:02:39 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome Abinash Patnaik !")
+    fmt.Fprintf(w, "Welcome Tomasso Amorossi !")
     fmt.Println("Endpoint Hit: homePage")
 }
 
