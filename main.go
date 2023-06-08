230585dd7f5ce98338baf9caf9de12efa5fe00fe
abinashp-github <95472074+abinashp-github@users.noreply.github.com>
2023-06-08 22:17:48 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
+//package main
+
+//import (
+//    "fmt"
+//    "log"
+//    "net/http"
+//)
+
+//func homePage(w http.ResponseWriter, r *http.Request){
+//    fmt.Fprintf(w, "Welcome Tomasso Amorossi !")
+//    fmt.Println("Endpoint Hit: homePage")
+//}
+
+//func handleRequests() {
+//    http.HandleFunc("/", homePage)
+//    log.Fatal(http.ListenAndServe(":9001", nil))
+//}
+
+//func main() {
+//    handleRequests()
+//    fmt.Println("Welcome Tomasso Amorossi !")
+//}
+
 package main
-
-import (
-    "fmt"
-    "log"
-    "net/http"
-)
-
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome Tomasso Amorossi !")
-    fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":9001", nil))
-}
-
+import "fmt"
 func main() {
-    handleRequests()
-    fmt.Println("Welcome Tomasso Amorossi !")
+    fmt.Println("hello world")
 }
